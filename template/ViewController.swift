8d8263dade1eeab081d8bdede83ff2684764cd29
Anders Borch <anders@cyborch.com>
2017-06-29 16:08:04 +0200

    Updated consent manager use.

--- template/ViewController.swift
+++ template/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
     // TODO: Replace client id in bundle and secret here with a real client id and secret.
     let resourceManager = ResourceManager(clientSecret: "7b66-4479-97d9-02d3253bb5c5")
     
-    var flowManager: ConsentFlowManager?
+    var consentManager: ConsentFlowManager?
+    var taskManager: TaskFlowManager?
     
     private func presentErrorWithOKButton(controller: UIViewController, title: String, message: String) {
         let alert = UIAlertController(title: title,
@@ -32,11 +33,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.resourceManager.survey(id: "d2dc0930-eed1-11e6-bd6f-f776b2d823f3") { (survey: Survey?, error: Error?) in
+        self.resourceManager.survey(id: "6a9fb9f0-139f-11e7-8358-e501073245bf") { (survey: Survey?, error: Error?) in
             guard error == nil else {
+                var title = NSLocalizedString("Error", comment: "Unknown Error title")
+                var message = NSLocalizedString("An error occurred, please try again later.", comment: "Unknown Error message")
+                if let response = (error as? NSError)?.userInfo[AFRKNetworkingOperationFailingURLResponseErrorKey] as? HTTPURLResponse {
+                    if response.statusCode == 404 {
+                        title = NSLocalizedString("Resource Not Found", comment: "404 Error title")
+                        message = NSLocalizedString("The survey could not be found. Contact support or try again later.", comment: "404 Error message")
+                    }
+                }
                 self.presentErrorWithOKButton(controller: self,
-                                              title: NSLocalizedString("Error", comment: "Unknown Error title"),
-                                              message: NSLocalizedString("An error occurred, please try again later.", comment: "Unknown Error message"))
+                                              title: title,
+                                              message: message)
                 return
             }
             guard survey != nil else {
@@ -45,18 +54,25 @@ class ViewController: UIViewController {
                                               message: NSLocalizedString("An error occurred, please try again later.", comment: "Survey Not Found error message"))
                 return
             }
-            self.flowManager = ConsentFlowManager(resourceManager: self.resourceManager, survey: survey!)
-            let vc = self.flowManager!.viewController
-            self.flowManager?.delegate.consentCompletion = { (_ controller: UIViewController, _ user: User?, authRefreshToken: String?, _ error: Error?) in
+
+            self.consentManager = ConsentFlowManager(resourceManager: self.resourceManager, survey: survey!)
+            self.taskManager = TaskFlowManager(resourceManager: self.resourceManager, survey: survey!)
+
+            let consentController = self.consentManager!.viewController
+            self.consentManager?.delegate.consentCompletion = { (_ controller: UIViewController, _ user: User?, authRefreshToken: String?, _ error: Error?) in
                 if let userError = error as? ResourceManager.UserCreationError {
                     self.presentErrorWithOKButton(controller: controller,
                                                   title: userError.localizedTitle,
                                                   message: userError.localizedDescription)
                 }
                 guard user != nil else { return }
-                vc.dismiss(animated: true, completion: nil)
+                consentController.dismiss(animated: true, completion: {
+                    guard let taskController = self.taskManager!.viewController(task: survey!.task) else { return }
+                    self.taskManager?.start(task: survey!.task)
+                    self.present(taskController, animated: false, completion: nil)
+                })
             }
-            self.present(vc, animated: false, completion: nil)
+            self.present(consentController, animated: false, completion: nil)
         }
     }
 
